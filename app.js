bbec9a2e4113efe92be8bd31e32a1ac195c275bf
ritajaCx <ritaja626@gmail.com>
2022-02-01 12:43:52 +0530

    changed port

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require("express")
 var app = express()
-var port = 3005 //changed port number
+var port = process.env.PORT || 3005 
 
 app.get("/", function(req, res){
     res.send("Welcome page")
@@ -9,6 +9,10 @@ app.get("/", function(req, res){
 app.get("/about", function(req, res){
     res.send("About us page")
 })
+app.get("/content", function(req, res){
+    res.send("Content us page")
+})
+
 
 app.listen(port, function(err){
     console.log("server started at port ", port)
